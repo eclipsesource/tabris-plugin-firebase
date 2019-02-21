092689e8c1f4d43403201df8e69e06a22a8c4e7f
Moritz Post <mpost@eclipsesource.com>
2019-02-21 09:45:56 +0100

    Add missing semicolons and use const instead of var

--- www/Messaging.js
+++ www/Messaging.js
@@ -1,12 +1,12 @@
-var EVENT_TYPES = ['tokenChanged', 'instanceIdChanged', 'message'];
+const EVENT_TYPES = ['tokenChanged', 'instanceIdChanged', 'message'];
 
-var readOnly = {
+const readOnly = {
   set: function(name) {
-    console.warn(`Can not set read-only property "${name}"`)
+    console.warn(`Can not set read-only property "${name}"`);
   }
 };
 
-var Messaging = tabris.NativeObject.extend('com.eclipsesource.firebase.Messaging');
+const Messaging = tabris.NativeObject.extend('com.eclipsesource.firebase.Messaging');
 
 Messaging.prototype._listen = function(name, listening) {
   if (EVENT_TYPES.indexOf(name) > -1) {
@@ -18,12 +18,12 @@ Messaging.prototype._listen = function(name, listening) {
 
 Messaging.prototype._dispose = function() {
   throw new Error('Messaging can not be disposed');
-}
+};
 
 Messaging.prototype.resetInstanceId = function() {
   this._nativeCall('resetInstanceId');
   return this;
-}
+};
 
 Messaging.prototype.requestPermissions = function() {
   if (tabris.device.platform === 'iOS') {
@@ -32,14 +32,14 @@ Messaging.prototype.requestPermissions = function() {
     console.warn('requestPermissions() is only supported on iOS.');
   }
   return this;
-}
+};
 
 Object.defineProperty(Messaging.prototype, 'pendingMessages', {
   get: function() {
     return {
       getAll: () => this._nativeCall('getAllPendingMessages') || [],
       clearAll: () => this._nativeCall('clearAllPendingMessages')
-    }
+    };
   }
 });
 
