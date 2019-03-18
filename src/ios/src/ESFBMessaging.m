f701a7bd66f34acc339478a3b7951d5577356658
Patryk Mól <pmol@eclipsesource.com>
2019-03-18 15:02:50 +0100

    Update ESFBMessaging
    This commits consists of 2 parts:
     - Part 1 is removal of old code which was required to support of older
       iOS versions (iOS 10 and lower)
     - Part 2 are bug fixes for `tokenChanged` event and `data`-type
       notifications not being sent to JS side
    
    Due to Tabris.js 3.x supporting iOS 11 and up there is no need to keep
    some of the code that sole purpose was compatibility with older iOS
    versions. This vastly simplifies the code and removes the need for
    `ESFBMessagingDelegate`. Sole purpose of this object was to cache
    incoming notifications for use by `ESFBMessaging` after it is finally
    instantiated by JS. With new `UNUserNotificationCenter` there is no need
    for this mechanism to exists.
    This code refactoring is extensive enough that following bugs were fixed
    alongside. Splitting them into separate commits at this point would
    have been artificatial and basically require them to reproduced after
    refactoring.
    
    Bug fixes were not separated due extensive changes in part one.
    Newer version of Firebase deprecated some of it’s methods. Updating code
    to use these new versions fixed `tokenChanged` events.
    Bug with `data`-type notification was fixed by moving following line to
    constructor:
    `[FIRMessaging messaging].shouldEstablishDirectChannel = YES;`
    Previously this line was in `registerForNotifications` method of
    `ESFBMessagingDelegate`.
    Other delegate assignments were also moved to constructor to avoid any
    potential problems.

--- src/ios/src/ESFBMessaging.m
+++ src/ios/src/ESFBMessaging.m
@@ -12,22 +12,7 @@
 #import <UserNotifications/UserNotifications.h>
 #import "ESFirebaseHelper.h"
 
-NSString *const kGCMMessageIDKey = @"gcm.message_id";
-
-@interface ESFBMessagingDelegate : NSObject <FIRMessagingDelegate>
-@property (weak) ESFBMessaging *instance;
-- (void)registerForNotifications;
-@end
-
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-#import <UserNotifications/UserNotifications.h>
-
-@interface ESFBMessagingDelegate () <UNUserNotificationCenterDelegate>
-@end
-#endif
-
-@interface ESFBMessaging ()
-- (void)sendMessage:(NSDictionary *)data;
+@interface ESFBMessaging () <UNUserNotificationCenterDelegate, FIRMessagingDelegate>
 @end
 
 @implementation ESFBMessaging
@@ -40,13 +25,15 @@ NSString *const kGCMMessageIDKey = @"gcm.message_id";
 @synthesize messageListener = _messageListener;
 @synthesize showBannersInForeground = _showBannersInForeground;
 
-static ESFBMessagingDelegate *messagingDelegate;
+NSString *const kGCMMessageIDKey = @"gcm.message_id";
 static NSDictionary *launchData;
 
 - (instancetype)initWithObjectId:(NSString *)objectId properties:(NSDictionary *)properties inContext:(id<TabrisContext>)context {
     self = [super initWithObjectId:objectId properties:properties inContext:context];
     if (self) {
-        messagingDelegate.instance = self;
+        [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+        [FIRMessaging messaging].delegate = self;
+        [FIRMessaging messaging].shouldEstablishDirectChannel = YES;
         [self registerSelector:@selector(resetInstanceId) forCall:@"resetInstanceId"];
         [self registerSelector:@selector(registerForNotifications) forCall:@"requestPermissions"];
         [self registerSelector:@selector(getAllPendingMessages) forCall:@"getAllPendingMessages"];
@@ -71,10 +58,6 @@ static NSDictionary *launchData;
     return @"com.eclipsesource.firebase.Messaging";
 }
 
-+ (void)notificationReceived:(NSDictionary *)data {
-    [messagingDelegate.instance sendMessage:data];
-}
-
 + (void)setLaunchData:(NSDictionary *)launchOptions {
     launchData = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
 }
@@ -89,12 +72,10 @@ static NSDictionary *launchData;
 
 + (void)setup {
     [ESFirebaseHelper setup];
-    messagingDelegate = [ESFBMessagingDelegate new];
 }
 
 - (NSArray *)getAllPendingMessages {
     __block NSArray *array;
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
     dispatch_group_t group = dispatch_group_create();
     dispatch_group_enter(group);
     [[UNUserNotificationCenter currentNotificationCenter] getDeliveredNotificationsWithCompletionHandler:^(NSArray<UNNotification *> * _Nonnull notifications) {
@@ -108,7 +89,6 @@ static NSDictionary *launchData;
         dispatch_group_leave(group);
     }];
     dispatch_group_wait(group, dispatch_time(DISPATCH_TIME_NOW,NSEC_PER_SEC * 1));
-#endif
     return array;
 }
 
@@ -125,7 +105,9 @@ static NSDictionary *launchData;
 }
 
 - (void)registerForNotifications {
-    [messagingDelegate registerForNotifications];
+    UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert | UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
+    [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:authOptions completionHandler:^(BOOL granted, NSError * _Nullable error) {}];
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
 }
 
 - (void)resetInstanceId {
@@ -136,12 +118,30 @@ static NSDictionary *launchData;
     [[FIRInstanceID instanceID] tokenWithAuthorizedEntity:[plist objectForKey:@"GCM_SENDER_ID"] scope:kFIRInstanceIDScopeFirebaseMessaging options:@{@"apns_token":apnsToken} handler:^(NSString * _Nullable token, NSError * _Nullable error) {
         if (!error && token) {
             __strong ESFBMessaging *strongSelf = weakSelf;
-            [strongSelf messaging:nil didRefreshRegistrationToken:token];
+            [strongSelf messaging:nil didReceiveRegistrationToken:token];
         }
     }];
 }
 
-- (void)messaging:(FIRMessaging *)messaging didRefreshRegistrationToken:(NSString *)fcmToken {
+- (void)sendMessage:(NSDictionary *)data {
+    if (self.messageListener) {
+        [self fireEventNamed:@"message" withAttributes:@{@"data":data}];
+    }
+}
+
+#pragma mark - UNUserNotificationCenterDelegate
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+    NSDictionary *userInfo = notification.request.content.userInfo;
+    [self sendMessage:userInfo];
+    UNNotificationPresentationOptions presentation = self.showBannersInForeground ? UNNotificationPresentationOptionBadge : UNNotificationPresentationOptionNone;
+    completionHandler(presentation);
+}
+
+#pragma mark - FIRMessagingDelegate
+
+- (void)messaging:(FIRMessaging *)messaging didReceiveRegistrationToken:(NSString *)fcmToken {
     if (self.tokenChangedListener) {
         [self fireEventNamed:@"tokenChanged" withAttributes:@{@"token":fcmToken}];
     }
@@ -156,60 +156,5 @@ static NSDictionary *launchData;
         [self fireEventNamed:@"instanceIdChanged" withAttributes:@{@"instanceId":[FIRInstanceID instanceID].token}];
     }
 }
-
-- (void)sendMessage:(NSDictionary *)data {
-    if (self.messageListener) {
-        [self fireEventNamed:@"message" withAttributes:@{@"data":data}];
-    }
-}
-
-@end
-
-@implementation ESFBMessagingDelegate
-
-- (void)registerForNotifications {
-    [FIRMessaging messaging].delegate = self;
-    [FIRMessaging messaging].shouldEstablishDirectChannel = YES;
-    if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_9_x_Max) {
-        UIUserNotificationType allNotificationTypes =
-        (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
-        UIUserNotificationSettings *settings =
-        [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
-        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
-    } else {
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-        [UNUserNotificationCenter currentNotificationCenter].delegate = self;
-        UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert | UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
-        [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:authOptions completionHandler:^(BOOL granted, NSError * _Nullable error) {}];
-#endif
-    }
-    [[UIApplication sharedApplication] registerForRemoteNotifications];
-}
-
-- (void)messaging:(FIRMessaging *)messaging didRefreshRegistrationToken:(NSString *)fcmToken {
-    [self.instance messaging:messaging didRefreshRegistrationToken:fcmToken];
-}
-
-- (void)messaging:(FIRMessaging *)messaging didReceiveMessage:(FIRMessagingRemoteMessage *)remoteMessage {
-     [self.instance messaging:messaging didReceiveMessage:remoteMessage];
-}
-
-- (void)instanceIdRefreshed:(NSNotification *)notification {
-    [self.instance instanceIdRefreshed:notification];
-}
-
-- (void)applicationReceivedRemoteMessage:(FIRMessagingRemoteMessage *)remoteMessage {
-    [self.instance messaging:nil didReceiveMessage:remoteMessage];
-}
-
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification
-         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
-    NSDictionary *userInfo = notification.request.content.userInfo;
-    [self.instance sendMessage:userInfo];
-    UNNotificationPresentationOptions presentation = self.instance.showBannersInForeground ? UNNotificationPresentationOptionBadge : UNNotificationPresentationOptionNone;
-    completionHandler(presentation);
-}
-#endif
 
 @end
